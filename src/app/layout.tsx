bbf94721f7fc2428d055a6af1a647c42a93e3464
Jeffy-Fung <92999261+Jeffy-Fung@users.noreply.github.com>
2025-01-26 16:33:34 +0800

    feat: new layout logic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Layout from "@/components/layout";
+import { isLogin } from "./helpers/auth";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +26,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {isLogin ? <Layout>{children}</Layout> : <div>{children}</div>}
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
-import SignIn from "@/components/sign-in";
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { isLogin } from "@/app/helpers/auth";
 
 export default function Home() {
-  return <SignIn />;
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isLogin) {
+      router.replace("/user-profile");
+    } else {
+      router.replace("/sign-in");
+    }
+  }, [router]);
+
+  return <div>Home</div>;
 }
+
