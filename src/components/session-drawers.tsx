13ccd1df691066eea8cd451b9cc041ab338132cd
Jeffy-Fung <jeffyfung0414@gmail.com>
2025-02-08 23:36:33 +0800

    refactor: prettier formatting

--- src/components/session-drawers.tsx
+++ src/components/session-drawers.tsx
@@ -1,26 +1,37 @@
-'use client'
+"use client";
 
-import { useContext } from 'react'
-import { Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
-import { XMarkIcon } from '@heroicons/react/24/outline'
-import ChatSessionContext from '@/store/ChatSessionProvider';
-import Divider from '@/components/divider';
-import DividerWithButton from '@/components/divider-with-button';
+import { useContext } from "react";
+import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import ChatSessionContext from "@/store/ChatSessionProvider";
+import Divider from "@/components/divider";
+import DividerWithButton from "@/components/divider-with-button";
 
 type Session = {
   id: string;
   date: string;
   action: () => void;
   current: boolean;
-}
+};
 
-export default function SessionDrawer({ sessions, postChatSession }: { sessions: Session[], postChatSession: () => void }) {
-  const { sessionDrawerOpen, setSessionDrawerOpen } = useContext(ChatSessionContext);
+export default function SessionDrawer({
+  sessions,
+  postChatSession,
+}: {
+  sessions: Session[];
+  postChatSession: () => void;
+}) {
+  const { sessionDrawerOpen, setSessionDrawerOpen } =
+    useContext(ChatSessionContext);
 
   // TODO: handle create session loading state
 
   return (
-    <Dialog open={sessionDrawerOpen} onClose={setSessionDrawerOpen} className="relative z-10">
+    <Dialog
+      open={sessionDrawerOpen}
+      onClose={setSessionDrawerOpen}
+      className="relative z-10"
+    >
       <div className="fixed inset-0" />
 
       <div className="fixed inset-0 overflow-hidden">
@@ -33,7 +44,9 @@ export default function SessionDrawer({ sessions, postChatSession }: { sessions:
               <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
                 <div className="px-4 sm:px-6">
                   <div className="flex items-start justify-between">
-                    <DialogTitle className="text-base font-semibold text-gray-900">Sessions</DialogTitle>
+                    <DialogTitle className="text-base font-semibold text-gray-900">
+                      Sessions
+                    </DialogTitle>
                     <div className="ml-3 flex h-7 items-center">
                       <button
                         type="button"
@@ -50,7 +63,10 @@ export default function SessionDrawer({ sessions, postChatSession }: { sessions:
                 <div className="relative mt-6 flex-1 px-4 sm:px-6">
                   <Divider />
                   <SessionDrawerContent sessions={sessions} />
-                  <DividerWithButton onClick={() => postChatSession()} buttonText="New Session" />
+                  <DividerWithButton
+                    onClick={() => postChatSession()}
+                    buttonText="New Session"
+                  />
                 </div>
               </div>
             </DialogPanel>
@@ -58,7 +74,7 @@ export default function SessionDrawer({ sessions, postChatSession }: { sessions:
         </div>
       </div>
     </Dialog>
-  )
+  );
 }
 
 function SessionDrawerContent({ sessions }: { sessions: Session[] }) {
@@ -68,20 +84,27 @@ function SessionDrawerContent({ sessions }: { sessions: Session[] }) {
         <li key={session.id}>
           <div className="group relative flex items-center px-5 py-6">
             <button onClick={session.action} className="-m-1 block flex-1 p-1">
-              <div aria-hidden="true" className="absolute inset-0 group-hover:bg-gray-50" />
+              <div
+                aria-hidden="true"
+                className="absolute inset-0 group-hover:bg-gray-50"
+              />
               <div className="relative flex min-w-0 flex-1 items-center">
                 <span className="relative inline-block shrink-0">
                   <span
                     aria-hidden="true"
                     className={classNames(
-                      session.current ? 'bg-green-400' : 'bg-gray-300',
-                      'absolute right-0 top-0 block size-2.5 rounded-full ring-2 ring-white',
+                      session.current ? "bg-green-400" : "bg-gray-300",
+                      "absolute right-0 top-0 block size-2.5 rounded-full ring-2 ring-white"
                     )}
                   />
                 </span>
                 <div className="ml-4 truncate">
-                  <p className="truncate text-sm font-medium text-gray-900">{session.id}</p>
-                  <p className="truncate text-sm text-gray-500">@{session.date}</p>
+                  <p className="truncate text-sm font-medium text-gray-900">
+                    {session.id}
+                  </p>
+                  <p className="truncate text-sm text-gray-500">
+                    @{session.date}
+                  </p>
                 </div>
               </div>
             </button>
@@ -89,7 +112,7 @@ function SessionDrawerContent({ sessions }: { sessions: Session[] }) {
         </li>
       ))}
     </ul>
-  )
+  );
 }
 
 function classNames(...classes: string[]): string {
