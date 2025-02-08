180001fad629a07117aeb74430b5238b040dc356
Jeffy-Fung <jeffyfung0414@gmail.com>
2025-02-09 00:12:18 +0800

    refactor: minor chatbot component formatting

--- src/components/chatbot.tsx
+++ src/components/chatbot.tsx
@@ -1,34 +1,42 @@
 import { getAuthToken } from "@/app/helpers/auth";
 import { DeepChat } from "deep-chat-react";
 
-export default function Chatbot({ histories, sessionId, path }: { histories: { role: string; text: string }[], sessionId: string, path: string }) {
+export default function Chatbot({
+  histories,
+  sessionId,
+  path,
+}: {
+  histories: { role: string; text: string }[];
+  sessionId: string;
+  path: string;
+}) {
   return (
     <div className="w-full h-full">
       <DeepChat
         connect={{
           url: `${process.env.NEXT_PUBLIC_NODEJS_BACKEND_API_URL}${path}`,
           method: "POST",
-          headers: { "Authorization": `Bearer ${getAuthToken()}` },
-          additionalBodyProps: { "sessionId": sessionId }
+          headers: { Authorization: `Bearer ${getAuthToken()}` },
+          additionalBodyProps: { sessionId: sessionId },
         }}
-      style={{
-        borderRadius: "10px",
-        width: "800px",
-        height: "700px",
-        paddingTop: "20px",
-        paddingBottom: "20px"
-      }}
-      textInput={{
-        placeholder: { text: "Welcome to the demo!" },
-        styles: {
-          container: {
-            borderRadius: "10px",
-            padding: "5px"
-          }
-        }
-      }}
-      history={histories}
-    />
+        style={{
+          borderRadius: "10px",
+          width: "800px",
+          height: "700px",
+          paddingTop: "20px",
+          paddingBottom: "20px",
+        }}
+        textInput={{
+          placeholder: { text: "Welcome to the demo!" },
+          styles: {
+            container: {
+              borderRadius: "10px",
+              padding: "5px",
+            },
+          },
+        }}
+        history={histories}
+      />
     </div>
   );
 }
