f9f3b54e8b3ee8717aaab280b5fc5c01d2f9430f
Jeffy-Fung <jeffyfung0414@gmail.com>
2025-02-09 13:06:16 +0800

    Revert "fix: reference error for building for deployment"
    This reverts commit 152567b31847719fbb8397e2e910d66f7b1d9908.

--- src/app/helpers/storage/index.ts
+++ src/app/helpers/storage/index.ts
@@ -1,18 +1,11 @@
 export function saveItem(key: string, value: string) {
-  if (typeof window !== 'undefined') {
-    localStorage.setItem(key, value);
-  }
+  localStorage.setItem(key, value);
 }
 
 export function getItem(key: string) {
-  if (typeof window !== 'undefined') {
-    return localStorage.getItem(key);
-  }
-  return null;
+  return localStorage.getItem(key);
 }
 
 export function removeItem(key: string) {
-  if (typeof window !== 'undefined') {
-    localStorage.removeItem(key);
-  }
+  localStorage.removeItem(key);
 }
